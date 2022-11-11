4db4919859959ab646ac3fc58529f838a7a3441a
Grzegorz Nawara <grzegorz.nawara@gmail.com>
2022-11-11 11:31:03 +0100

    Fido prototype

---
+++ fido/src/components/Fidomap.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { cssStyles } from '../config/AppConfig'
+//import debug from '../debug'
+
+const Fidomap = ({ title, subtitle, score, look, linkTo }) => (
+  <div className="fidomap-container">
+    <img src="./images/map1.png" />
+  </div>
+)
+
+export default Fidomap
